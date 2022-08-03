b3aa89b4681ec6a54cd4f42eb11371216bfff8ae
Fran <franco_kuperman@hotmail.com>
2022-08-03 18:08:34 -0300

    todosIguales

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -113,13 +113,29 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var cajita = 1;
+  if (arguments.length ===0){
+      return 0;
+  } else if (arguments.length === 1){
+      return arguments[0]
+  } else {
+    for (var i = 0; i < arguments.length; i++){
+      cajita = cajita * arguments[i]
+    } 
+  }
+      return cajita;
 }
-
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+var cajita = 0;
+for (var i = 0; i < arreglo.length; i++){
+  if (arreglo[i] > 18){
+    cajita += 1;
+  }
+}
+  return cajita;
 }
 
 
@@ -128,7 +144,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7){
+    return "Es fin de semana"
+  }else if (numeroDeDia > 1 && numeroDeDia < 7){
+    return "Es dia Laboral"
+  }
 } 
 
 
@@ -136,7 +156,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  var cambio = n.toString();
+  if (cambio[0] === '9'){
+    return true;
+  } return false;
 }
 
 

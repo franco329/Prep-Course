8cd4500f859d625c84c90030567729ee852c85c2
Fran <franco_kuperman@hotmail.com>
2022-08-23 12:26:46 -0300

    test JSX_Finalizado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -109,12 +109,20 @@ function sortArray(arr) {
 }
 
 
+//    arreglo1: [4, 2, 3]
+//
+//    key:       0  1  2
+//
+//    arreglo2: [1, 3, 4]
+
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var nuevoArray = arreglo1.filter(x => arreglo2.indexOf(x) !== -1)
+  return nuevoArray;
 }
 
 
